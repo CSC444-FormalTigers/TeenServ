d4e2497e20685f60e1c00d5a0b5bd72642b087ef
Hanbin Sock <hbsock@gmail.com>
2017-09-26 21:28:45 -0400

    Add controller for User model.
    Set up routing for the CRUD operations on users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :users
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
