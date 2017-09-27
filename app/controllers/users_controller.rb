54169dc4f0f88e0a54c21131e422ad7d76b32af1
Hanbin Sock <hbsock@gmail.com>
2017-09-26 22:01:14 -0400

    Implement creation and reading of users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
   end
 
+  def create
+    @user = User.new(user_params)
 
+    @user.save
+    redirect_to @user
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :email)
+    end
 end
