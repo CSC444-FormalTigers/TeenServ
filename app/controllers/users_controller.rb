74e310bdd398a5dbf9e1b95ca7518e1b76fd26bc
Hanbin Sock <hbsock@gmail.com>
2017-09-29 19:09:45 -0400

    Add deletion of Users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :password, :email)
