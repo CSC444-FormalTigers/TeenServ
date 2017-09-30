b64e1b3226e0cbdef38bff9102f7af097761a7b5
Hanbin Sock <hbsock@gmail.com>
2017-09-29 20:21:02 -0400

    Move test out of controller to integration.
    The test was for creating a new user, which is testing that the controller
    can create a model from requests. This is not a unit test.

---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "can create a user" do
+    get "/users/new"
+    assert_response :success
+
+    post "/users",
+      params: { user: {
+        username: "PossibleUserName",
+        password: "PossiblePassword",
+        email: "Possible@Email.com"}}
+
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "Username:\n  PossibleUserName"
+  end
+
+end
