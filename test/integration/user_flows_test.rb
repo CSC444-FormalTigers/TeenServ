1c3defbaadcdbf211c1ec8c6e474b3d880bd6817
Hanbin Sock <hbsock@gmail.com>
2017-10-10 19:40:06 -0400

    Add tests for CRUD operations on users.

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -5,6 +5,11 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  test "can get index of users" do
+    get "/users"
+    assert_response :success
+  end
+
   test "can create a user" do
     get "/users/new"
     assert_response :success
@@ -16,8 +21,35 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
         email: "Possible@Email.com"}}
 
     assert_response :redirect
-    follow_redirect!
+  end
+
+  test "can show a user" do
+    user = users(:one)
+    get user_url(user)
     assert_response :success
+  end
+
+  test "can edit a user" do
+    user = users(:one)
+    get edit_user_url(user)
+    assert_response :success
+
+    patch user_url(user),
+      params: { user: {
+      username: "PossibleUserName",
+      password: "PossiblePassword",
+      email: "Possible@Email.com"}}
+    assert_response :redirect
+
+  end
+
+  test "can delete a user" do
+    user = users(:one)
+    assert_difference('User.count', -1) do
+      delete user_url(user)
+    end
+
+    assert_redirected_to users_path
   end
 
 end
