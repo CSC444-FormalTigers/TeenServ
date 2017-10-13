1b51b8735fc8b842d2f0b49617214b755c54180d
Hanbin Sock <hbsock@gmail.com>
2017-10-13 11:17:09 -0400

    Set up email to be send by gmail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,19 @@ Rails.application.configure do
 
   #Default url
   config.action_mailer.default_url_options = { host: 'teen-serv.herokuapp.com' }
-  
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => 'gmail.com'
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+
 end
