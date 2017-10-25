9638ea5257719c6833e4ebb3b48aeec5519f3880
Alex Bath <bathalex210@gmail.com>
2017-10-25 13:27:05 -0400

    Added basic homepage styling.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:index]
   def index
+    @Jobs = Job.all
+    @average_wage = Job.average(:hourly_pay)
+    @Users = User.all
   end
 end
