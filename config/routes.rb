e4617df98d0b438fe9a7df9817c8489cd5afc346
Hanbin Sock <hbsock@gmail.com>
2017-10-28 18:21:10 -0400

    Add empty controller JobApplicationsController.

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :jobs
+  resources :jobs do
+    resources :job_applications
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
