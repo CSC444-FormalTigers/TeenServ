66b0e1229ff5a3c3caa3c5292662de1da4dbae3d
Hanbin Sock <hbsock@gmail.com>
2017-10-29 17:27:38 -0400

    Add delete tests for job applications.

--- test/controllers/job_applications_controller_test.rb
+++ test/controllers/job_applications_controller_test.rb
@@ -7,6 +7,11 @@ class JobApplicationsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @job = jobs(:one)
+
+    @job_application = job_applications(:one)
+    @job_application.job = @job
+    assert @job_application.save
+
   end
 
 
@@ -15,8 +20,13 @@ class JobApplicationsControllerTest < ActionDispatch::IntegrationTest
       post job_job_applications_url(@job), params: {job_application: {
 	      applicant_username: @job.username}}
     end
+    assert_redirected_to job_url(@job)
   end
 
-  #test "can be deleted"
-  #end
+  test "can be deleted" do 
+    assert_difference('JobApplication.count', -1) do
+      delete job_job_application_url(@job, @job_application)
+    end
+    assert_redirected_to job_url(@job)
+  end
 end
