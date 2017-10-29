eb5cb82a26bbeb49ca325887206d07b13e4fa493
Hanbin Sock <hbsock@gmail.com>
2017-10-29 16:12:18 -0400

    Add tests for job applications.

--- test/controllers/job_applications_controller_test.rb
+++ test/controllers/job_applications_controller_test.rb
@@ -4,4 +4,20 @@ class JobApplicationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @job = jobs(:one)
+  end
+
+
+ # test "can be created" do
+ #   assert_difference 'JobApplication.count' do
+ #     post job_job_applications_url(@job), params: {job_application: {
+ #             job_id: @job.id,
+ #             applicant_username: @job.username}}
+ #   end
+ # end
+
+  #test "can be deleted"
+  #end
 end
--- test/models/job_application_test.rb
+++ test/models/job_application_test.rb
@@ -4,4 +4,10 @@ class JobApplicationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "can save a job application" do
+    job_application = JobApplication.new({
+            applicant_username: "someUsername"})
+    assert job_application.save
+  end
 end
