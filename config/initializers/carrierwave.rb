e59a12843222a737d070aa46dafee9226231f30b
Jaime Iturriaga <jaime.iturriaga@mail.utoronto.ca>
2017-11-02 13:56:04 -0400

    Pushed fog decleration before config.sorge = :fog This should fix the production issue.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,16 @@
 CarrierWave.configure do |config|
 
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => 'AKIAJ7WIXBXGS2V33LVQ',            # required
+    :aws_secret_access_key  => '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',     # required
+    :region                 => 'eu-east-2'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'teenserv'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
@@ -13,15 +24,4 @@ CarrierWave.configure do |config|
       config.storage = :fog
     end
   end
-
-  config.fog_credentials = {
-    :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'AKIAJ7WIXBXGS2V33LVQ',            # required
-    :aws_secret_access_key  => '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',     # required
-    :region                 => 'eu-east-2'                        # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'teenserv'               # required
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
