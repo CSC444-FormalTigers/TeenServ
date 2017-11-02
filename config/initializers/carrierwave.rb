b3bc7393db574f9531dcae7b71e3d3ea5883b2e7
Jaime Iturriaga <jaime.iturriaga@mail.utoronto.ca>
2017-11-02 15:32:43 -0400

    Removed storage declaration in uploader. This may have been overwriting the config declaration, so I removed it.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,8 @@
 CarrierWave.configure do |config|
 
-  config.fog_credentials = {
-    :provider               => 'AWS',                             # required
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {                          # required
+    :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAJ7WIXBXGS2V33LVQ',            # required
     :aws_secret_access_key  => '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',     # required
     :region                 => 'eu-east-2'                        # optional, defaults to 'us-east-1'
