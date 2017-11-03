f8c891196df7e63aa0babb1c3d5f3fe1f3615ab9
Jaime Iturriaga <jaime.iturriaga@mail.utoronto.ca>
2017-11-03 13:45:01 -0400

    Enabled logger for tests. Now you can see the output logs for tests. The default log level is ERROR which shows error messages. If you want general info, set your environment variable LOG_LEVEL to :INFO. Example: rake test LOG_LEVEL=info

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Enable stdout loger
+  config.logger = Logger.new(STDOUT)
+
+  #Set log level
+  config.log_level = ENV.fetch("LOG_LEVEL","ERROR")
 end
