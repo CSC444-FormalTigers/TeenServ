67c5d839bec5ddc643f99cd0a106a28813e08c24
Jaime Iturriaga <jaime.iturriaga@mail.utoronto.ca>
2017-11-04 12:02:11 -0400

    Added validations to check for file type

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,6 +13,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def content_type_whitelist
+    %w(image/*)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
