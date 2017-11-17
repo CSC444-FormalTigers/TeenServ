55201675028d67d4cab287544ca5fd5e8fe97ea4
Hanbin Sock <hbsock@gmail.com>
2017-11-16 22:33:49 -0500

    Move the javascript in jobs/show.
    Forgot to do so from previous commit.

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -111,3 +111,36 @@ function initJobNewMap() {
 }
 
 
+
+var geocoder;
+
+function initJobShowMap() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(43.6532, 79.3832);
+    var mapOptions = {
+        zoom: 13,
+        center: latlng
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    // Call the codeAddress function (once) when the map is idle (ready)
+    google.maps.event.addListenerOnce(map, 'idle', codeAddress);
+}
+function codeAddress() {
+    console.log(address);
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            // Center map on location
+            map.setCenter(results[0].geometry.location);
+            // Add marker on location
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
+
