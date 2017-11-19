3aaf1c6c6105f799fdb5d9d4fd499130af35733d
Jaime Iturriaga <jaime.iturriaga@mail.utoronto.ca>
2017-11-19 16:22:42 -0500

    Fixed paypal configuration issue. Since api credentials are stored as environment variables, need to configure it using the initializer rather than through a YAML file.

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,9 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
+#PayPal::SDK.load("config/paypal.yml", Rails.env)
 PayPal::SDK.logger = Rails.logger
+
+PayPal::SDK.configure(
+  :mode      => "sandbox",
+  :app_id    => "APP-80W284485P519543T",
+  :username  => ENV["PAYPAL_USERNAME"],
+  :password  => ENV["PAYPAL_PASSWORD"],
+  :signature => ENV["PAYPAL_SIGNATURE"] )
