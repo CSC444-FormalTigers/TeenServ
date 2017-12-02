2bb06509fe2c19dbc869ca0086c77cbedd6387ed
Daniel Hu <daniel.huxc@gmail.com>
2017-12-02 02:45:14 -0500

    update admin test

--- test/controllers/superadmin_controller_test.rb
+++ test/controllers/superadmin_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class SuperadminControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test "should not get admin page if while not logged in" do
     get superadmin_url
-    assert_response :success
+    assert_redirected_to root_path
   end
 
 end
